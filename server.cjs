4174002a38fcca43412461ce8a46d694e6b2be3b
ychetra <chetra.storeit@gmail.com>
2025-01-23 15:35:41 +0700

    update serve hosting

--- server.cjs
+++ server.cjs
@@ -1,26 +1,22 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-import { networkInterfaces } from 'os';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const express = require('express');
+const path = require('path');
+const os = require('os');
 
 const app = express();
 const port = 3000;
 const ip = '0.0.0.0'; // Listen on all network interfaces
 
 // Serve static files from the dist directory
-app.use(express.static(join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle all routes by serving index.html
 app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, ip, () => {
     // Get local IP address
-    const nets = networkInterfaces();
+    const nets = os.networkInterfaces();
     const localIPs = [];
     
     for (const name of Object.keys(nets)) {
